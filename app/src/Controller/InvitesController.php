d22e2188323e43662df95f590bdec42fc88647c8
Daniil Popov <warcatyt@gmail.com>
2023-01-15 16:28:32 +0300

    Оптимизация страницы приглашений

--- app/src/Controller/InvitesController.php
+++ app/src/Controller/InvitesController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\ResumeRepository;
+use App\Repository\VacancyRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,8 +11,29 @@ use Symfony\Component\Routing\Annotation\Route;
 class InvitesController extends AbstractController
 {
     #[Route('/invites', name: 'invites')]
-    public function index(): Response
+    public function index(ResumeRepository $resumeRepository, VacancyRepository $vacancyRepository): Response
     {
-        return $this->render('invites/index.html.twig');
+        $userRoles = $this->getUser() !== null ? $this->getUser()->getRoles() : null;
+
+        if ($this->getUser()) {
+            if (in_array('ROLE_SEEKER', $userRoles)) {
+                $role = 'seeker';
+                $title = 'Вас пригласили на вакансии:';
+
+                $resumes = $resumeRepository->findBy(['seeker' => $this->getUser()]);
+            } elseif (in_array('ROLE_RECRUITER', $userRoles)) {
+                $role = 'recruiter';
+                $title = 'Вы пригласили на вакансии:';
+
+                $vacancies = $vacancyRepository->findBy(['recruiter' => $this->getUser()]);
+            }
+        }
+
+        return $this->render('invites/index.html.twig', [
+            'title' => $title ?? 'Отклики',
+            'role' => $role ?? 'Гость',
+            'resumes' => $resumes ?? [],
+            'vacancies' => $vacancies ?? [],
+        ]);
     }
 }
