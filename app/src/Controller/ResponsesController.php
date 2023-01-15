79d6145f87cd96807f4574e51b2f01ea8f390e11
Daniil Popov <warcatyt@gmail.com>
2023-01-15 16:16:10 +0300

    Оптимизация страницы откликов

--- app/src/Controller/ResponsesController.php
+++ app/src/Controller/ResponsesController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\ResumeRepository;
+use App\Repository\VacancyRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,8 +11,29 @@ use Symfony\Component\Routing\Annotation\Route;
 class ResponsesController extends AbstractController
 {
     #[Route('/responses', name: 'responses')]
-    public function index(): Response
+    public function index(ResumeRepository $resumeRepository, VacancyRepository $vacancyRepository): Response
     {
-        return $this->render('responses/index.html.twig');
+        $userRoles = $this->getUser() !== null ? $this->getUser()->getRoles() : null;
+
+        if ($this->getUser()) {
+            if (in_array('ROLE_SEEKER', $userRoles)) {
+                $role = 'seeker';
+                $title = 'Вы откликнулись на вакансии:';
+
+                $resumes = $resumeRepository->findBy(['seeker' => $this->getUser()]);
+            } elseif (in_array('ROLE_RECRUITER', $userRoles)) {
+                $role = 'recruiter';
+                $title = 'На ваши вакансии откликнулись:';
+
+                $vacancies = $vacancyRepository->findBy(['recruiter' => $this->getUser()]);
+            }
+        }
+
+        return $this->render('responses/index.html.twig', [
+            'title' => $title ?? 'Отклики',
+            'role' => $role ?? 'Гость',
+            'resumes' => $resumes ?? [],
+            'vacancies' => $vacancies ?? [],
+        ]);
     }
 }
